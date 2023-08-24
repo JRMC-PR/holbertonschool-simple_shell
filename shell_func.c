82a48e43fd8c36b87d14a0f17bf60db7ef420804
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-08-24 19:37:03 +0000

    memmory fixin

--- shell_func.c
+++ shell_func.c
@@ -16,6 +16,7 @@ void non_inter(char *com, char **env)
 	if (child == 0)
 	{
 		execve(Tokens[0], Tokens, env); /* Execute the com */
+		free(Tokens);
 		perror("error: "); /* if execve fails */
 		exit(EXIT_FAILURE); /* Exit child with failure status */
 	}
